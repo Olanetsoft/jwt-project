35e0c4539e90ef10daa42747abb51e56be8a7821
Senninseyi <seyi.oyebamiji@gmail.com>
2022-09-17 06:03:41 +0100

    Update user.js

--- model/user.js
+++ model/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
+  displayName: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
