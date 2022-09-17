f15833c6f090e8c9664b3a785c11addb256f154d
Senninseyi <seyi.oyebamiji@gmail.com>
2022-09-17 06:03:37 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const { MONGO_URI } = process.env;
+const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
   // Connecting to the database
   mongoose
-    .connect(MONGO_URI, {
+    .connect(MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
